2a74a73d80008b33a578389de453ccfdd1a49fa4
36725950 <36725950+Ray-Hackshaw@users.noreply.github.com>
2020-04-05 17:27:51 +1200

    Fixing moblie compatability issues.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function getRandomInt(min, max) {
 function fontChangerStart() {
   fontChangerStart.interval = setTimeout(function(){ fontChangerStart(); }, 75);
   var font_array = ["Helvetica", "Courier", "Florence", "Times New Roman", "Arial", "Georgia", "Geneva", "Tahoma", "Trebuchet MS", "Verdana"];
-  var word_array = ["farewell", "adieu", "arrivederci", "sayonara", "au revoir", "ardievas", "soraidh slàn", "despedida", "afscheid", "goodbye"];
+  var word_array = ["farewell", "adieu", "arrivederci", "sayonara", "au-revoir", "ardievas", "soraidh slàn", "despedida", "afscheid", "goodbye"];
   var random = getRandomInt(0, 9);
    // document.getElementById("first").style.fontFamily = font_array[getRandomInt(0, 9)];
    // document.getElementById("second").style.fontFamily = font_array[getRandomInt(0, 9)];
